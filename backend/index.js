126ce19386eeff9d3c6bcc792f156a50848b8589
huzaifawork <mhuzaifatariq7@gmail.com>
2023-12-16 22:19:14 +0500

    backend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,13 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 .catch(()=> console.log('not conncted'));
 
 app.listen(5000);
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', products);
