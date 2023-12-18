c457218de2ade0662bd68485f507f58e12fc264e
huzaifawork <mhuzaifatariq7@gmail.com>
2023-12-18 14:38:48 +0500

    style

--- midwork/src/components/Aboutus.js
+++ midwork/src/components/Aboutus.js
@@ -9,8 +9,8 @@ function Aboutus() {
       </div>
 
       <div className="hero-section">
-        <h2>Welcome To Our Shop</h2>
-        <p style={{ color: 'black' }}>
+        <h2 style={{marginLeft:"600px"}}>Welcome To Our Shop</h2>
+        <p style={{ color: 'black' , marginLeft:"600px"}}>
           This shop is dedicated to providing you with the best selection of items related to style, fashion, and sports. We've created a user-friendly environment to ensure you have the best shopping experience, including a hassle-free returns and refund policy. Feel free to share your reviews of the items you purchase here. We offer worldwide delivery to your specified location, and we accept a variety of international payment methods. <b>Thank You</b>
         </p>
       </div>
